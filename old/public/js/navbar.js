e899a0dbede3a871f51929907bf9ca4e3aaa5367
JoshuaHolloway <jhollow6@asu.edu>
2021-02-19 21:52:02 -0600

    transfer of User-Interface-II repo (branch: josh-holloway2) into User-Interface-III repo

---
+++ old/public/js/navbar.js
@@ -0,0 +1,9 @@
+const openSlideMenu = () => {
+    document.getElementById('Side-Menu').style.width = '250px';
+    console.log('.Side-Menu pressed');
+}
+
+const closeSlideMenu = () => {
+    document.getElementById('Side-Menu').style.width = '0px';
+    console.log('.Btn-Close pressed');
+}
