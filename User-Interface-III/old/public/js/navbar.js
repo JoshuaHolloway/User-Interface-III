0f141cdd13e9c10152666d92ed5813906ed3fe67
JoshuaHolloway <jhollow6@asu.edu>
2021-02-27 19:56:49 -0600

    removed III

--- User-Interface-III/old/public/js/navbar.js
+++
@@ -1,9 +0,0 @@
-const openSlideMenu = () => {
-    document.getElementById('Side-Menu').style.width = '250px';
-    console.log('.Side-Menu pressed');
-}
-
-const closeSlideMenu = () => {
-    document.getElementById('Side-Menu').style.width = '0px';
-    console.log('.Btn-Close pressed');
-}
